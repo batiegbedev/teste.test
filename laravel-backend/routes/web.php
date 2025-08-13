50223812c8fae3dc56b5df0c18611fdeba372105
batiegbedev <bategbecoulibaly2003@gmail.com>
2025-08-13 23:49:40 +0000

    bonjour

---
+++ laravel-backend/routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Define your web routes here
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Additional web routes can be added below
